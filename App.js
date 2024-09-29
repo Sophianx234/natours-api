7c50b2765351a67fbf879d848ab96e847d82fe05
Damian X <dx4336969@gmail.com>
2024-09-29 13:14:47 +0000

    responding to url params

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ const port = 3000;
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8')
 );
+app.use(express.json())
 
 app.get('/api/v1/tours', (req, res) => {
   res.json({
@@ -15,6 +16,38 @@ app.get('/api/v1/tours', (req, res) => {
     }
   });
 });
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params)
+    const id = +req.params.id
+    if(id>tours.length) return res.status(404).json({
+        status: 'failed',
+        message: 'invalid id'
+    })
+    const tour = tours.find(tour=>tour.id === id)
+    console.log(tour)
+    res.status(200).json({
+        status: 'success',
+        data :{
+            tour: tour
+        }
+    })
+  
+});
+
+app.post('/api/v1/tours',(req,res)=>{
+    const newId = tours.length +1
+    const newTour = Object.assign({id: newId},req.body)
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours.json`,JSON.stringify(tours),err=>{
+        res.status(201).json({
+            status: 'success',
+            data:{
+
+                tour: newTour
+            } 
+        })
+    })
+})
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
