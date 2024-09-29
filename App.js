f514bcb42238c3ebdf90605f3fe0ed4220c5f77f
Damian X <dx4336969@gmail.com>
2024-09-29 08:12:33 +0000

    handling get request

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
-app.get('/',(req,res)=>{
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.json({
+    status: 'success',
+    data: {
+        tours: tours
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+/* app.get('/',(req,res)=>{
     res.status(200).json({
     message: 'Hello from the server ',
     App: 'Natours'
     })
 })
-app.listen(port, ()=>{
-    console.log(`App running on port ${port}`)
-})
+
+app.post('/',(req,res)=>{
+    res.send('You can post to this end ')
+
+}) */
