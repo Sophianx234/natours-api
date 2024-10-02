6ce5e08405694c4b01ecb65b078fa2279e5076ab
Damian X <dx4336969@gmail.com>
2024-10-02 10:05:58 +0000

    stop tracknig

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan("dev"))
 
 }
+
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 app.use('/api/v1/tours',tourRouter )
