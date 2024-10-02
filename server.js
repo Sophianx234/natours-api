f2b45f0ac73d9b3a6147579de784297ac4abaf1b
Damian X <dx4336969@gmail.com>
2024-10-02 10:26:34 +0000

    setting up Schema

--- server.js
+++ server.js
@@ -15,6 +15,18 @@ mongoose.connect(DB,{
   console.log('DB connected successfully')
 })
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: Number, required: [true, 'a tour must have a name', unique: true]
+  },
+  rating: {type: Number, required: true , default: 4.5},
+  price: {
+    type: Number,
+  }
+
+});
+
+const Tours = mongoose.model('Tour',tourSchema) 
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
