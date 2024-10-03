6649a882104f581de438317438934fac0b5a5e32
Damian X <dx4336969@gmail.com>
2024-10-03 07:00:37 +0000

    modelling tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,12 +1,37 @@
 const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
     name: {
-      type: String, required: [true, 'a tour must have a name'], unique: true
+      type: String, required: [true, 'A tour must have a name'], unique: true
     },
-    rating: {type: Number, required: true , default: 4.5},
+    duration: {type: Number, required: [true, "A tour must have a duration"]},
+    maxGroupSize:{type: Number, required: [true, "A tour must have a max group size"]},
+    difficulty: {type: String, required: [true, 'A tour must have a difficulty']},
+    ratingsAverage: {type: Number,  default: 4.5},
+    ratingsQuantity: {type: Number,  default: 0},
     price: {
-      type: Number
-    }
+      type: Number,
+      required: [true, 'A tour must have a price']
+    },
+    priceDiscount:Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true,'A tour must have a summary'] 
+    },
+    descrption:{
+      type: String,
+      trim: true
+    },
+    imageCover:{
+      type: String,
+      required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    startDates: [Date]
   
   });
   
