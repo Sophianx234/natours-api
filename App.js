23b2e657ce08559ac3562bcf3d070c762b34b46d
Damian X <dx4336969@gmail.com>
2024-10-08 14:36:50 +0000

    applying global error handling middleware

--- App.js
+++ App.js
@@ -15,11 +15,19 @@ app.use(express.static(`${__dirname}/public`))
 app.use('/api/v1/tours',tourRouter )
 app.use('/api/v1/users',usersRouter)
 app.all('*',(req,res,next)=>{
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server`
+   const err = new Error(`Can't find ${req.originalUrl} on this server`)
+   err.status = 'fail';
+   err.statusCode = 404
+    next(err);
+})
+app.use((err,req,res,next)=>{
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status||'fail'
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
     })
-    next();
+    next()
 })
 
 
