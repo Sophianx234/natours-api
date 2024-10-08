ed9d436b68908f3d3d6e24fbbee16bd22ddfde61
Damian X <dx4336969@gmail.com>
2024-10-08 14:21:00 +0000

    add handle unhandled route feature

--- App.js
+++ App.js
@@ -14,6 +14,13 @@ app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 app.use('/api/v1/tours',tourRouter )
 app.use('/api/v1/users',usersRouter)
+app.all('*',(req,res,next)=>{
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server`
+    })
+    next();
+})
 
 
 
